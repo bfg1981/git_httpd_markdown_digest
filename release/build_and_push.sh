36d8854ac28e1b2239476a5ce0898a4679536658
BFG <Tom.Fredrik@blenning.no>
2024-04-24 10:24:04 +0200

    Added release script

---
+++ release/build_and_push.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+TAG_NAME=therealbfg/git_httpd_markdown_digest
+
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+
+if [ ! -f $SCRIPT_DIR/TAG_NAME ]
+then
+  echo "You need to write the name of the tag in '$SCRIPT_DIR/TAG_NAME'"
+  exit 1
+fi
+
+TAG_NAME=$(cat $SCRIPT_DIR/TAG_NAME)
+
+fname=$(mktemp)
+
+docker build --pull . | tee $fname
+
+TAG_ID=$(tail -n 1 $fname | rev | cut -d\  -f1 | rev)
+rm $fname
+
+docker tag $TAG_ID $TAG_NAME
+echo Tagged $TAG_NAME
+docker push $TAG_NAME
